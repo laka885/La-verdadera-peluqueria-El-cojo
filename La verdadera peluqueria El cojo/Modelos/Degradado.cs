ddd58055ab1ae6b3a2d6c826e292034f1fb54213
Julian Vazquez <juliandavivazquezsantana@gmail.com>
2026-04-09 21:50:06 -0400

    Polimorfismo en clase degradado

---
+++ La verdadera peluqueria El cojo/Modelos/Degradado.cs
@@ -0,0 +1,8 @@
+﻿namespace PeluqueriaElCojo.Modelos
+{
+    public class Degradado : Servicio
+    {
+        public int Nivel { get; set; } // 1, 2 o 3
+        public override decimal CalcularPrecio() => PrecioBase + (Nivel * 50);
+    }
+}
