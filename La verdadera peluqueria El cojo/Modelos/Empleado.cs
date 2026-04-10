b063fa9b2be1477097dc31397545e514113f0ed7
Julian Vazquez <juliandavivazquezsantana@gmail.com>
2026-04-09 22:05:10 -0400

    Modulo de empleados

---
+++ La verdadera peluqueria El cojo/Modelos/Empleado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using PeluqueriaElCojo.Atributos;
+
+namespace PeluqueriaElCojo.Modelos
+{
+    public class Empleado : IComparable<Empleado>
+    {
+        [Requerido]
+        public string Nombre { get; set; }
+        public decimal VentasAcumuladas { get; set; }
+
+        // Requisito: Ordenar por ventas de mayor a menor
+        public int CompareTo(Empleado otro)
+        {
+            if (otro == null) return 1;
+            return otro.VentasAcumuladas.CompareTo(this.VentasAcumuladas);
+        }
+    }
+}
