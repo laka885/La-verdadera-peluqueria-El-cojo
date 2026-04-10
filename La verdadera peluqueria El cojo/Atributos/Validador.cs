23895539a961834b5d22750f54c3aa78b8ee69f1
Julian Vazquez <juliandavivazquezsantana@gmail.com>
2026-04-09 22:12:56 -0400

    sistema de validacion y modulo de inventario

---
+++ La verdadera peluqueria El cojo/Atributos/Validador.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using PeluqueriaElCojo.Atributos;
+
+namespace PeluqueriaElCojo.Utilidades
+{
+    public static class Validador
+    {
+        public static List<string> Validar(object obj)
+        {
+            var errores = new List<string>();
+            if (obj == null) return errores;
+
+            // Reflection: obtenemos las propiedades del objeto en tiempo de ejecución
+            PropertyInfo[] propiedades = obj.GetType().GetProperties();
+
+            foreach (var prop in propiedades)
+            {
+                // Buscamos si la propiedad tiene atributos de validación
+                object[] atributos = prop.GetCustomAttributes(typeof(ValidacionAttribute), true);
+
+                foreach (object attr in atributos)
+                {
+                    ValidacionAttribute validacion = attr as ValidacionAttribute;
+                    if (validacion != null)
+                    {
+                        object valor = prop.GetValue(obj, null);
+                        if (!validacion.EsValido(valor))
+                        {
+                            errores.Add(validacion.MensajeError ?? "Dato inválido en " + prop.Name);
+                        }
+                    }
+                }
+            }
+            return errores;
+        }
+    }
+}
---
+++ La verdadera peluqueria El cojo/Modelos/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using PeluqueriaElCojo.Atributos;
+
+namespace PeluqueriaElCojo.Modelos
+{
+    public class Producto : IEquatable<Producto>, ICloneable
+    {
+        [Requerido(MensajeError = "El código es obligatorio")]
+        public string Codigo { get; set; }
+
+        [Requerido]
+        public string Nombre { get; set; }
+
+        [Rango(1, 10000)]
+        public decimal Precio { get; set; }
+
+        public int Stock { get; set; }
+
+        // Requisito: Detectar duplicados por código
+        public bool Equals(Producto otro)
+        {
+            if (otro == null) return false;
+            return this.Codigo == otro.Codigo;
+        }
+
+        // Requisito: Clonar productos
+        public object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+
+        public override bool Equals(object obj) => Equals(obj as Producto);
+        public override int GetHashCode() => (Codigo != null ? Codigo.GetHashCode() : 0);
+    }
+}
