4b0c107660528e04c9a99c56afa0fa3877706509
Julian Vazquez <juliandavivazquezsantana@gmail.com>
2026-04-09 20:54:30 -0400

    Agregar archivos de proyecto.

---
+++ La verdadera peluqueria El cojo/Form1.cs
@@ -0,0 +1,10 @@
+namespace La_verdadera_peluqueria_El_cojo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
